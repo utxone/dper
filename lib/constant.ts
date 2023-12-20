9f831e9f819861545ec4d8af28a2cf0bde57b9ae
stonega <xijieyin@gmail.com>
2023-12-21 03:02:42 +0800

    fix: env

--- lib/constant.ts
+++ lib/constant.ts
@@ -1,3 +1,3 @@
-export const TESTNET = process.env.NEXT_PUBLIC_TESTNET! === "true";
+export const TESTNET = process.env.NEXT_PUBLIC_TESTNET! == "true";
 export const HEIGHT = Number(process.env.NEXT_PUBLIC_HEIGHT!);
 export const RECEIVER = process.env.NEXT_PUBLIC_RECEIVER!;
