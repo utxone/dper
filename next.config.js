9e5d7c6d6965e125cbdd6682e72aaa092c69fd01
stonega <xijieyin@gmail.com>
2024-02-03 21:33:02 +0800

    fix: minor fix

--- next.config.js
+++ next.config.js
@@ -4,6 +4,6 @@ const withPWA = require("@ducanh2912/next-pwa").default({
   });
   
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = withPWA({})
 
 module.exports = nextConfig
